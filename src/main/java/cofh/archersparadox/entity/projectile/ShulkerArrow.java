6c5c3f463658d6e78573c2868544424f2667d5e7
King Lemming <kinglemming@gmail.com>
2023-07-10 16:29:49 -0700

    Bump version.

--- src/main/java/cofh/archersparadox/entity/projectile/ShulkerArrow.java
+++ src/main/java/cofh/archersparadox/entity/projectile/ShulkerArrow.java
@@ -18,7 +18,6 @@ import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.network.NetworkHooks;
